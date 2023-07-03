e7a94301e7ee9844e4cd7dc5b940d77809885b34
GlebAQA <galievgleb99@gmail.com>
2023-07-03 16:06:35 +0300

    create hot fix test lab

--- main_project/pages/smoke_test.py
+++ main_project/pages/smoke_test.py
@@ -1,22 +1,17 @@
 import time
-import pyautogui
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 from seleniumbase import SB
 
 with SB(headed=True) as driver:
     driver.open('http://localhost:5173/#/sensors?pageIndex=undefined/')
-    # driver.maximize_window()
-    # driver.click('div.menu-item')
-    # time.sleep(2)
-    time.sleep(1)
-    pyautogui.keyDown('alt')
-    time.sleep(0.5)
-    pyautogui.keyDown('s')
-    time.sleep(0.5)
-    pyautogui.keyDown('1')
-    time.sleep(2)
-    pyautogui.keyUp('alt')
-    pyautogui.keyUp('s')
-    pyautogui.keyUp('1')
+
+    action_chains = ActionChains(driver.driver)
+    action_chains.key_down(Keys.ALT)
+    action_chains.send_keys('s')
+    action_chains.send_keys('2')
+    action_chains.key_up(Keys.ALT)
+    action_chains.perform()
 
     time.sleep(5)
-
