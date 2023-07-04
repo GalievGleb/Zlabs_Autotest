685bcc200729f23263a7ec92e9c1072b234196b1
GlebAQA <galievgleb99@gmail.com>
2023-07-04 21:05:46 +0300

    add corrected working code

--- main_project/test_use_pypetter.py
+++ main_project/test_use_pypetter.py
@@ -1,56 +1,57 @@
 from PIL import Image, ImageDraw
 from selenium import webdriver
 import os
-import sys
 
 
 class ScreenAnalysis:
     STAGING_URL = 'http://www.yahoo.com'
     PRODUCTION_URL = 'http://www.yahoo.com'
-    driver = None
 
     def __init__(self):
-        self.set_up()
+        self.driver = self.set_up()
         self.capture_screens()
         self.analyze()
         self.clean_up()
 
     def set_up(self):
-        self.driver = webdriver.Chrome()
+        driver = webdriver.Chrome()
+        return driver
 
     def clean_up(self):
-        self.driver.close()
+        self.driver.quit()
 
     def capture_screens(self):
         self.screenshot(self.STAGING_URL, 'screen_staging.png')
         self.screenshot(self.PRODUCTION_URL, 'screen_production.png')
 
     def screenshot(self, url, file_name):
-        print
-        "Capturing", url, "screenshot as", file_name, "..."
+        print("Capturing", url, "screenshot as", file_name, "...")
         self.driver.get(url)
         self.driver.set_window_size(1024, 768)
-        self.driver.save_screenshot(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'screenshots', file_name))
-        self.driver.get_screenshot_as_png()
-        print
-        "Done."
+        screenshot_path = os.path.join('screenshots', file_name)
+        self.driver.save_screenshot(screenshot_path)
+        print("Done.")
 
     def analyze(self):
-        screenshot_staging = Image.open("screenshots/screen_staging.png")
-        screenshot_production = Image.open("screenshots/screen_production.png")
+        screenshot_staging = Image.open(os.path.join('screenshots', 'screen_staging.png'))
+        screenshot_production = Image.open(os.path.join('screenshots', 'screen_production.png'))
         columns = 60
         rows = 80
         screen_width, screen_height = screenshot_staging.size
 
-        block_width = ((screen_width - 1) // columns) + 1  # this is just a division ceiling
-        block_height = ((screen_height - 1) // rows) + 1
+        block_width = (screen_width - 1) // columns + 1  # this is just a division ceiling
+        block_height = (screen_height - 1) // rows + 1
 
         for y in range(0, screen_height, block_height + 1):
             for x in range(0, screen_width, block_width + 1):
                 region_staging = self.process_region(screenshot_staging, x, y, block_width, block_height)
                 region_production = self.process_region(screenshot_production, x, y, block_width, block_height)
 
-                if region_staging is not None and region_production is not None and region_production != region_staging:
+                if (
+                        region_staging is not None
+                        and region_production is not None
+                        and region_production != region_staging
+                ):
                     draw = ImageDraw.Draw(screenshot_staging)
                     draw.rectangle((x, y, x + block_width, y + block_height), outline="red")
 
@@ -68,9 +69,8 @@ class ScreenAnalysis:
                     pixel = image.getpixel((coordinateX, coordinateY))
                     region_total += sum(pixel) / 4
                 except:
-                    return
+                    return None
 
         return region_total / factor
-
 
 ScreenAnalysis()
